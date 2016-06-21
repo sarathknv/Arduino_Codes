e85363abaf171faa66612fee185d3679ddde5e7f
Sarath31 <sarathchandra.knv31@gmail.com>
2016-06-21 22:17:55 +0530

    added comments

--- RGBled_hex_input/RGBled_hex_input.ino
+++ RGBled_hex_input/RGBled_hex_input.ino
@@ -1,4 +1,8 @@
-int value[6];
+/*Enter the HEXADECIMAL color code(proper) in the Serial monitor and watch the RGB led display the color
+   Ex: #FF00AA - FF red, 00 green, AA blue
+*/
+
+int value[6]; //0, 1 for red, 2, 3 for green, 4, 5 for blue
 char c;
 
 int rPin = 11;
@@ -17,9 +21,9 @@ void setup() {
 
 void loop() {
   while (Serial.available() == 0);
-  delay(100);
+  delay(100);   //give time to buffer
 
-  getColorCode();
+  getColorCode();   //sores the entered data in values[]
   setColor((value[0] * 16 + value[1]), (value[2] * 16 + value[3]), (value[4] * 16 + value[5]));
   //setColor(255, 0, 0);
 }
