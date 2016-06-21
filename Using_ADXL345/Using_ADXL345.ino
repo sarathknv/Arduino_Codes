41bd61eb806ab41695ebe62018b40b701a5375f2
Sarath31 <sarathchandra.knv31@gmail.com>
2016-06-21 14:33:56 +0530

    added hardware offsets

--- Using_ADXL345/Using_ADXL345.ino
+++ Using_ADXL345/Using_ADXL345.ino
@@ -1,15 +1,15 @@
 /*
- I2C communication protocol
- ADXL345         Arduino Uno/Nano
+  I2C communication protocol
+  ADXL345         Arduino Uno/Nano
    GND             GND
    Vin             3.3V
    SDA             A4
    SDO             A5
-Reference: 
+  Reference:
   http://codeyoung.blogspot.in/2009/11/adxl345-accelerometer-breakout-board.html
   http://morf.lv/modules.php?name=tutorials&lasit=31
   http://www.analog.com/media/en/technical-documentation/data-sheets/ADXL345.pdf
-  
+
 */
 #include<Wire.h>
 
@@ -17,17 +17,16 @@ Reference:
 const int numBytes = 6;  //Number of bytes to receive(two bytes for each axis)
 
 byte values[6];          //To store the data received
-int rawx,rawy,rawz;      
+int rawx, rawy, rawz;
 
-typedef struct{
+typedef struct {
   double x;
   double y;
   double z;
-}accel;
-
+} accel;
 accel a;
 
-void setup(){
+void setup() {
   Wire.begin();
   Serial.begin(9600);
 
@@ -35,19 +34,20 @@ void setup(){
   writeTo(DEVICE, 0x2D, 16);
   writeTo(DEVICE, 0x2D, 8);
   writeTo(DEVICE, 0x31, 11);  // 11 for 13 bit resolution and ±16 g
+  init(60, 45, 127);
 }
 
-void loop(){
+void loop() {
   readFrom(DEVICE, 0x32, numBytes, values);
   rawx = (((int)values[1]) << 8) | values[0];
   rawy = (((int)values[3]) << 8) | values[2];
   rawz = (((int)values[5]) << 8) | values[4];
 
-a.x = rawx*0.00390625;
-a.y = rawy*0.00390625;
-a.z = rawz*0.00390625;
+  a.x = rawx * 0.00390625;
+  a.y = rawy * 0.00390625;
+  a.z = rawz * 0.00390625;
 
-  Serial.print(a.x, 4); Serial.print("  "); 
+  Serial.print(a.x, 4); Serial.print("  ");
   Serial.print(a.y, 4); Serial.print("  "); Serial.println(a.z, 4);
   delay(30);
 }
---
+++ Using_ADXL345/offsets.ino
@@ -0,0 +1,12 @@
+/*The offset registers are each 8 bit. I used a 13 bit resoulution with 3.90625 mg/LSB, but for offset adjustment
+   scale factor is 15.6 mg/LSB.
+   Also z axis acceleration was around 5m/s^2 and could not be adjusted by hardware offset because z_offset was 340 and 
+    is out of range for a byte.
+ */
+void init(byte x_offset, byte y_offset, byte z_offset)
+{
+  writeTo(DEVICE, 0x1E, x_offset);
+  writeTo(DEVICE, 0x1F, y_offset);
+  writeTo(DEVICE, 0x20, z_offset);
+}
+
