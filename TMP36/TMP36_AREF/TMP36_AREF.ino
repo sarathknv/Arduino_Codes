def67f5ab30a8dfdbe860183765cb714404354d9
Sarath31 <sarathchandra.knv31@gmail.com>
2016-06-22 20:36:48 +0530

    temp sensor with AREF

---
+++ TMP36/TMP36_AREF/TMP36_AREF.ino
@@ -0,0 +1,28 @@
+/*Using TMP36 Temperature sensor with AREF pin on arduino for more precision
+ * Reference: https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
+ *            http://tronixstuff.com/2013/12/12/arduino-tutorials-chapter-22-aref-pin/
+ */
+
+ 
+#define AREF_voltage 3.3
+int tempPin = 0;  //analog pin to be read from
+unsigned int rawReading; //from pin
+float tempVoltage;   //rawReading to voltage
+float tempC;       //actual temp in Celcius
+
+void setup(){
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+  //analogReference(DEFAULT);  //in case you want to revert back to board's voltage.
+}
+
+void loop(){
+  rawReading = analogRead(tempPin);
+  tempVoltage = (rawReading * (AREF_voltage))/1024;  //convert to voltage
+  tempC = (tempVoltage - 0.5)*100;      //TMP36 has an offset 500mV and 10mV per degree
+
+  Serial.print(tempVoltage);Serial.print("  ");
+  Serial.println(tempC);
+  delay(1000);
+}
+
