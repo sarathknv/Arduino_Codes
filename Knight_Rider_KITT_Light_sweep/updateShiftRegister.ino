76c12315f05e78833312c8ae49efdfcbb6f9b1fe
Sarath31 <sarathchandra.knv31@gmail.com>
2016-06-24 18:51:35 +0530

    added light sweep

--- Knight_Rider_KITT_Light_sweep/updateShiftRegister.ino
+++
@@ -1,8 +0,0 @@
-//update the shift register by writing the new value
-
-void updateShiftRegister(byte value)
-{
-   digitalWrite(latchPin, LOW); //pull latch pin LOW to start sending data
-   shiftOut(dataPin, clockPin, LSBFIRST, value); //send data
-   digitalWrite(latchPin, HIGH); //pull latch pin HIGH to stop sending data
-}
